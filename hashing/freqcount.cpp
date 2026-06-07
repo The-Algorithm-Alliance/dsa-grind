ee7c42815cc2f579fac5a4d6474e1613f86242ce
Advij07 <advaithvijayasarathy@gmail.com>
2026-06-07 14:20:53 +0530

    Update freqcount.cpp

--- hashing/freqcount.cpp
+++ hashing/freqcount.cpp
@@ -1,3 +1,5 @@
+//count the number of occurences of array elements using an unordered_map. 
+
 #include <iostream>
 #include <unordered_map>
 //#include <bits/stdc++.h> can be used as well but it runs a large overhead.
